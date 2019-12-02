54a6b053c9c56c372a8ea3618153e4fe20a8932e
TheLordPhantom <TheLordPhantom@users.noreply.github.com>
2019-12-02 23:32:56 +0200

    Initial commit

---
+++ BrainFuckInterpreter/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BrainFuckInterpreter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pRegistry = new System.Windows.Forms.Panel();
+            this.rtbBrainFuckCode = new System.Windows.Forms.RichTextBox();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.bRun = new System.Windows.Forms.Button();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // pRegistry
+            // 
+            this.pRegistry.AutoScroll = true;
+            this.pRegistry.Location = new System.Drawing.Point(12, 12);
+            this.pRegistry.Name = "pRegistry";
+            this.pRegistry.Size = new System.Drawing.Size(776, 103);
+            this.pRegistry.TabIndex = 0;
+            // 
+            // rtbBrainFuckCode
+            // 
+            this.rtbBrainFuckCode.Location = new System.Drawing.Point(12, 148);
+            this.rtbBrainFuckCode.Name = "rtbBrainFuckCode";
+            this.rtbBrainFuckCode.Size = new System.Drawing.Size(776, 290);
+            this.rtbBrainFuckCode.TabIndex = 1;
+            this.rtbBrainFuckCode.Text = "";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(12, 121);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(695, 20);
+            this.tbInput.TabIndex = 2;
+            // 
+            // bRun
+            // 
+            this.bRun.Location = new System.Drawing.Point(713, 119);
+            this.bRun.Name = "bRun";
+            this.bRun.Size = new System.Drawing.Size(75, 23);
+            this.bRun.TabIndex = 3;
+            this.bRun.Text = "Run";
+            this.bRun.UseVisualStyleBackColor = true;
+            this.bRun.Click += new System.EventHandler(this.bRun_Click);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(12, 444);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(776, 100);
+            this.rtbOutput.TabIndex = 4;
+            this.rtbOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 549);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.bRun);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.rtbBrainFuckCode);
+            this.Controls.Add(this.pRegistry);
+            this.Name = "Form1";
+            this.Text = "BrainFuck";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pRegistry;
+        private System.Windows.Forms.RichTextBox rtbBrainFuckCode;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button bRun;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+    }
+}
+
---
+++ BrainFuckInterpreter/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrainFuckInterpreter
+{
+    public partial class Form1 : Form
+    {
+        public static int regLen = 16;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Font regIndFont = new Font("Arial", 20);
+        Font regValFont = new Font("Arial", 15);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < regLen; i++) {
+                TextBox pRegInd = new TextBox();
+                pRegInd.Top = 3;
+                pRegInd.Width = 40;
+                pRegInd.Height = 40;
+                pRegInd.Left = 3 + (i * 43);
+                pRegInd.Text = i.ToString();
+                pRegInd.Font = regIndFont;
+                pRegInd.ReadOnly = true;
+                pRegInd.TextAlign = HorizontalAlignment.Center;
+
+                TextBox pRegVal = new TextBox();
+                pRegVal.Top = 49;
+                pRegVal.Width = 40;
+                pRegVal.Height = 40;
+                pRegVal.Left = 3 + (i * 43);
+                pRegVal.Text = "000";
+                pRegVal.Font = regValFont;
+                pRegVal.ReadOnly = true;
+
+                pRegistry.Controls.Add(pRegInd);
+                pRegistry.Controls.Add(pRegVal);
+
+                memory[i] = 0;
+                memoryView[i] = pRegVal;
+            }
+
+        }
+
+        TextBox[] memoryView = new TextBox[regLen];
+        byte[] memory = new byte[regLen];
+
+        void WriteRegistry()
+        {
+            for (int i = 0; i < regLen; i++)
+            {
+                memoryView[i].Text = memory[i].ToString();
+            }
+        }
+        public char[] SubArray(char[] data, int index, int length)
+        {
+            char[] result = new char[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
+        }
+        char[] subLoop(char[] AllCode, int codeInd)
+        {
+            int startInd = 0;
+            int endInd = 0;
+            for (int i = codeInd; i < AllCode.Length; i++)
+            {
+                if (AllCode[i] == '[') { startInd = i+1; break; }
+            }
+            int amtOpenBrack = 0;
+            for (int i = startInd+1; i < AllCode.Length; i++)
+            {
+                if (AllCode[i] == '[') { amtOpenBrack++; }
+                    if (AllCode[i] == ']') { if (amtOpenBrack == 0) { endInd = i; break; } else { amtOpenBrack--; } }
+            }
+            return SubArray(AllCode, startInd, endInd - startInd);
+        }
+
+        string runCode(char[] code, char[] input, int regInd, int codeInd, int inputInd)
+        {
+            //int regInd = 0;
+            //int codeInd = 0;
+            //int inputInd = 0;
+
+            string output = "";
+            while (codeInd < code.Length)
+            {
+                if (code[codeInd] == '+') { if (memory[regInd] < 255) { memory[regInd]++; } else if (memory[regInd] == 255) { memory[regInd] = 0; } }
+                if (code[codeInd] == '-') { if (memory[regInd] > 0) { memory[regInd]--; } else if (memory[regInd] == 0) { memory[regInd] = 255; } }
+                if (code[codeInd] == '>') { if (regInd != regLen - 1) { regInd++; } else { regInd = 0; } }
+                if (code[codeInd] == '<') { if (regInd > 0) { regInd--; } else { regInd = regLen - 1; } }
+                if (code[codeInd] == '.') { output += (char)memory[regInd]; }
+                if (code[codeInd] == ',') { memory[regInd] = (byte)input[inputInd]; inputInd++; }
+                if (code[codeInd] == '[') {
+                    char[] subL = subLoop(code, codeInd);
+                    while (memory[regInd] != 0) {
+                        runCode(subL,input,regInd,0,inputInd);
+                    }
+                    codeInd += subL.Length+1;
+                }
+                codeInd++;
+                WriteRegistry();
+            }
+
+            return output;
+        }
+
+        void ResetRegisrty()
+        {
+            for (int i = 0; i < regLen; i++)
+            {
+                memory[i] = 0;
+            }
+        }
+
+        private void bRun_Click(object sender, EventArgs e)
+        {
+            ResetRegisrty();
+            rtbOutput.Text = runCode(rtbBrainFuckCode.Text.ToArray(), tbInput.Text.ToArray(),0,0,0);
+        }
+    }
+}
